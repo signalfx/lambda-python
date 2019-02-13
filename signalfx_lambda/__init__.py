2446523c1b55bfad3ffe211c5dc7d39d000d7e06
Ashwin Chandrasekar <achandrasekar@signalfx.com>
2019-02-13 11:34:40 -0500

    Add convenience function to init

--- signalfx_lambda/__init__.py
+++ signalfx_lambda/__init__.py
@@ -3,7 +3,11 @@ import os
 import datetime
 
 from . import metrics
+from . import tracing
 from .version import name, version
 
 def wrapper(*args, **kwargs):
     return metrics.wrapper(*args, **kwargs)
+
+def tracing_wrapper(*args, **kwargs):
+    return tracing.wrapper(*args, **kwargs)
