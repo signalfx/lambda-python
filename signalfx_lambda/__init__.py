0cef7af72e9d2d68f1d6d7469382764c9552d3c9
Ashwin Chandrasekar <achandrasekar@signalfx.com>
2019-02-20 10:59:47 -0500

    Update convenience methods

--- signalfx_lambda/__init__.py
+++ signalfx_lambda/__init__.py
@@ -6,8 +6,26 @@ from . import metrics
 from . import tracing
 from .version import name, version
 
-def wrapper(*args, **kwargs):
+
+def metrics_wrapper(*args, **kwargs):
     return metrics.wrapper(*args, **kwargs)
+
 
 def tracing_wrapper(*args, **kwargs):
     return tracing.wrapper(*args, **kwargs)
+
+
+# less convenient method
+def send_metric(counters=[], gauges=[]):
+    metrics.send_metric(counters, gauges)
+
+
+# convenience method
+def send_counter(metric_name, metric_value=1, dimensions={}):
+    metrics.send_counter(metric_name, metric_value, dimensions)
+
+
+# convenience method
+def send_gauge(metric_name, metric_value, dimensions={}):
+    metrics.send_counter(metric_name, metric_value, dimensions)
+
